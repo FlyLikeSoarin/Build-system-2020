af1e856b6c599e870edd5b29bceebb5ea343fa7f
Vihrev Evgenij <evgeny.vihrev@gmail.com>
2020-12-15 19:43:22 +0300

    remove CMakeFiles I accidentally commited

--- CMakeFiles/PrepareA.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "A/index.h"
-  "CMakeFiles/PrepareA"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PrepareA.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
